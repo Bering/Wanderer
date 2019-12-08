b6dc0b0b0626cded65de221b607ffedc3e2d45cb
Bering <bering@ringlogic.com>
2019-12-08 12:01:22 -0500

    // instead of /, working viewport in jump screen and renamed vars

--- game.py
+++ game.py
@@ -46,31 +46,33 @@ class Game:
     def cmd_jump(self):
         
         cols, lines = shutil.get_terminal_size()
-        min_x = max(self.player.world_x - (cols / 2), 1)
-        min_x = min(min_x, self.config.world_width - cols)
-        max_x = min_x + cols - 1
+        view_left = max(self.player.world_x - (cols // 2), 1)
+        view_left = min(view_left, self.config.world_width - cols)
+        view_right = view_left + cols - 1
 
-        min_y = max(self.player.world_y - (lines / 2), 1)
-        min_y = min(min_y, self.config.world_height - lines)
-        max_y = min_y + lines - 1
+        view_top = max(self.player.world_y - (lines // 2), 1)
+        view_top = min(view_top, self.config.world_height - lines)
+        view_bottom = view_top + lines - 1
 
         # clear screen
         print('\x1b[2J')
 
         #print("Window size: " + str(cols) + "x" + str(lines))
-        #print("Viewing: (" + str(min_x) + "," + str(min_y) + ") - (" + str(max_x) + "," + str(max_y) + ")")
+        #print("Viewing: (" + str(view_left) + "," + str(view_top) + ") - (" + str(view_right) + "," + str(view_bottom) + ")")
 
         for s in self.world.stars:
-            if s.world_x < min_x or s.world_x > max_x \
-            or s.world_y < min_y or s.world_y > max_y:
+            if s.world_x < view_left or s.world_x > view_right \
+            or s.world_y < view_top or s.world_y > view_bottom:
                 continue
 
-            sv_x = s.world_x - min_x + 1
-            sv_y = s.world_y - min_y + 1
-            print('\x1b[' + str(sv_y) + ';' + str(sv_x) + 'H' + s.style + s.color + '*', end='')
+            col = s.world_x - view_left + 1
+            line = s.world_y - view_top + 1
+            print('\x1b[' + str(line) + ';' + str(col) + 'H' + s.style + s.color + '*', end='')
         
+        col = self.player.world_x - view_left + 1
+        line = self.player.world_y - view_top + 1
         print(
-            '\x1b[' + str(self.player.world_y - min_y + 1) + ';' + str(self.player.world_x - min_x + 1) + 'H' + 
+            '\x1b[' + str(line) + ';' + str(col) + 'H' + 
             colorama.Style.BRIGHT + 
             colorama.Fore.WHITE + 
             '@'
@@ -79,11 +81,12 @@ class Game:
         print(colorama.Style.RESET_ALL);
 
         k = '?'
-        cursor_x = self.player.world_x - min_x + 1
-        cursor_y = self.player.world_y - min_y + 1
         while(ord(k) != 13 and k != ' ' and ord(k) != 27):
 
+            cursor_x = col + view_left - 1
+            cursor_y = line + view_top - 1
             target_text = '(' + str(cursor_x) + ',' + str(cursor_y) + ')'
+            target = None
             for s in self.world.stars:
                 if s.world_x == cursor_x and s.world_y == cursor_y:
                     target_text = s.name
@@ -91,21 +94,21 @@ class Game:
                     break
 
             print('\x1b[' + str(lines) + ';0H' + 'Jump to: ' + '\x1b[0J' + target_text, end='', flush=True)
-            print('\x1b[' + str(cursor_y) + ';' + str(cursor_x) + 'H', end='', flush=True)
+            print('\x1b[' + str(line) + ';' + str(col) + 'H', end='', flush=True)
             
             k = getch.getch()
             if k == 'w':
-                if cursor_y > 1:
-                    cursor_y -= 1
+                if line > 1:
+                    line -= 1
             elif k == 'a':
-                if cursor_x > 1:
-                    cursor_x -= 1
+                if col > 1:
+                    col -= 1
             elif k == 'd':
-                if cursor_x < max_x:
-                    cursor_x += 1
+                if col < cols:
+                    col += 1
             elif k == 's':
-                if cursor_y < max_y:
-                    cursor_y += 1
+                if line < lines - 1:
+                    line += 1
         
         if ord(k) == 27:
             print('\x1b[' + str(lines) + ';0H' + '\n' + 'Jump CANCELED')
@@ -116,6 +119,9 @@ class Game:
         # TODO: random event or something :-)
         print('Jump successful!')
 
+        self.player.world_x = cursor_x
+        self.player.world_y = cursor_y
+        self.player.star = target
 
     def cmd_test(self):
         cols, lines = shutil.get_terminal_size()
--- player.py
+++ player.py
@@ -13,8 +13,8 @@ class Player:
         # self.world_y = random.randrange(config.world_height)
         self.world_x = 1
         self.world_y = 1
-        self.system_x = config.system_width / 5
-        self.system_y = config.system_height / 5
+        self.system_x = config.system_width // 5
+        self.system_y = config.system_height // 5
         self.ground_x = 1
         self.ground_y = 1
 
