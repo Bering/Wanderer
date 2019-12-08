811a5d131c34b6bc55c55ab95f9ada90399224ba
Bering <bering@ringlogic.com>
2019-12-07 22:20:26 -0500

    WIP. Using os.get_terminal_size() and renamed navigate to jump.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import os
 import getch
 import colorama
 import config
@@ -17,17 +18,6 @@ class Application:
 		
 		colorama.init()
 
-	def print_world(self, config, player):
-		min_x = player.ship.world_x - (config.view_width / 2)
-		max_x = player.ship.world_x + (config.view_width / 2)
-		min_y = player.ship.world_y - (config.view_height / 2)
-		max_y = player.ship.world_y + (config.view_height / 2)
-		for s in self.world.stars:
-			if s.world_x >= min_x and s.world_x <= max_x and s.world_y >= min_y and s.world_y <= max_y:
-				print(s.color +  '\x1b[' + s.world_y + ';' + s.world_x + 'H' + '*', end='')
-		
-		print(colorama.Style.RESET_ALL);
-
 	def run(self):
 		k = "?"
 		while(k != 'q' and ord(k) != 27):
@@ -36,8 +26,10 @@ class Application:
 
 			if k == 'q' or ord(k) == 27:
 				self.cmd_quit()
-			elif k == 'n':
-				self.cmd_navigate()
+			elif k == 'j':
+				self.cmd_jump()
+			elif k == 't':
+				self.cmd_test()
 			else:
 				print("Unknown or unavailable command: " + k)
 		
@@ -47,12 +39,37 @@ class Application:
 	def cmd_quit(self):
 		print("Quit")
 
-	def cmd_navigate(self):
-		print('Navigate to:')
-		print(' 1) Altair')
-		print(' 2) Aldebaran')
-		print(' 3) Betelgeuse')
-		print(' 4) Deneb')
+	def cmd_jump(self):
+		
+		# clear screen
+		print('\x1b[2J')
+
+		cols, lines = os.get_terminal_size()
+		min_x = self.player.world_x - (cols / 2)
+		max_x = self.player.world_x + (cols / 2)
+		min_y = self.player.world_y - (lines / 2)
+		max_y = self.player.world_y + (lines / 2)
+
+		print("World size: " + str(config.world_width) + "," + str(config.world_height))
+		print("Player position: " + str(self.player.world_x) + "," + str(self.player.world_y))
+		print("Window size: " + str(cols) + "," + str(lines))
+		print(str(min_x) + "," + str(max_x) + " - " + str(min_y) + "," + str(max_y))
+
+		for s in self.world.stars:
+			if s.world_x < min_x or s.world_x > max_x \
+			or s.world_y < min_y or s.world_y > max_y:
+				continue
+
+			sv_x = s.world_x - self.player.world_x
+			sv_y = s.world_y - self.player.world_y
+			print('\x1b[' + str(sv_y) + ';' + str(sv_x) + 'H' + s.color + '*', end='')
+		
+		print(colorama.Style.RESET_ALL);
+		print('\x1b[' + str(lines) + ';0H' + 'Jump to: ')
+
+	def cmd_test(self):
+		print('\x1b[2J')
+		print('\x1b[0;0H' + 'Test')
 
 
 print("Wanderer v.alpha0")
--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 # World (where the stars are)
-world_width = 640
-world_height = 480
+world_width = 320
+world_height = 96
 
 # Star System (where the planets are)
 system_width = 128
 system_height = 128
 min_planets_per_star = 2
 max_planets_per_star = 8
-
-# Size of the console window
-view_width = 80
-view_height = 23
