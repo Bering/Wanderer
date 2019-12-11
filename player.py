d638bddaa560499dfd8db0c519ece565af49104f
Bering <bering@ringlogic.com>
2019-12-11 07:43:16 -0500

    Player spawns in the middle of the world

--- player.py
+++ player.py
@@ -9,14 +9,12 @@ class Environment(Enum):
 class Player:
 
     def __init__(self, config):
-        # self.world_x = random.randrange(config.world_width)
-        # self.world_y = random.randrange(config.world_height)
-        self.world_x = 1
-        self.world_y = 1
-        self.system_x = config.system_width // 5
-        self.system_y = config.system_height // 5
-        self.ground_x = 1
-        self.ground_y = 1
+        self.world_x = config.world_width // 2
+        self.world_y = config.world_height // 2
+        self.system_x = 0
+        self.system_y = 0
+        self.ground_x = 0
+        self.ground_y = 0
 
         self.environment = Environment.WORLD
         self.star = None
