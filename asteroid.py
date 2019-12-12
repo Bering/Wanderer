4414997183b125ddf69b50f74c6149ac75234e49
Bering <bering@ringlogic.com>
2019-12-11 21:50:09 -0500

    Better naming convention for asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import random
+import string
 from colorama import Fore
 
 from body import Body
@@ -6,6 +7,15 @@ from body import Body
 class Asteroid(Body):
 
     def __init__(self, star, angle, distance):
-        name = 'Asteroid ' + str(random.randint(500,2000))
+        name = 'Asteroid (' + \
+                str(random.randint(0, 500000)) + \
+                ') ' + \
+                str(random.randint(2000,2200)) + \
+                ' ' + \
+                random.choice(string.ascii_letters).upper() + \
+                random.choice(string.ascii_letters).upper() + \
+                str(random.randint(0,9)) + \
+                str(random.randint(0,9))
+        
         super().__init__(star, name, angle, distance, Fore.WHITE, '.')
         
