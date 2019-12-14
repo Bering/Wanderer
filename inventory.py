b22a134ed7ab806cf9ef06a6a82e17249dac8241
Bering <bering@ringlogic.com>
2019-12-14 10:59:58 -0500

    Rewrote inventory and stock of trade stations now respect size_max

--- inventory.py
+++ inventory.py
@@ -1,74 +1,64 @@
 
+
 class InventoryItem:
-    def __init__(self, name, price, unit_size, quantity):
+    def __init__(self, name, unit_size, quantity):
         self.name = name
-        self.price = price
         self.unit_size = unit_size
         self.quantity = quantity
+
+
+class WouldOverflowError(Exception):
+    def __init__(self, name, unit_size, quantity, size_available):
+        self.name = name
+        self.unit_size = unit_size
+        self.quantity = quantity
+        self.size_available = size_available
+
+
+class ItemNotInStockError(Exception):
+    def __init__(self, name):
+        self.name = name
+
+
+class WouldUnderflowError(Exception):
+    def __init__(self, name, quantity_stocked):
+        self.name = name
+        self.quantity_stocked = quantity_stocked
 
 
 class Inventory:
     def __init__(self, size):
         self.size_max = size
         self.size = 0
-        self.items = []
+        self.items = {}
 
 
-    def buy(name, price, unit_size, quantity):
+    def add(self, name, unit_size, quantity):
+
         if self.size + (unit_size * quantity) > self.size_max:
-            # TODO: throw error instead
-            return
-
-        item = None
-        for i in self.items:
-            if i.name == name and i.price == price:
-                item = i
-                break
+            raise WouldOverflowError(name, unit_size, quantity, self.size_max - self.size)
         
-        if item == None:
-            self.items.append(name, price, unit_size, quantity)
+        if name in self.items:
+            self.items[name].quantity += quantity
         else:
-            item.quantity += quantity
+            self.items[name] = InventoryItem(name, unit_size, quantity)
 
         self.size += (unit_size * quantity)
-        return (prize * quantity)
 
     
-    def sell(name, quantity, size, price):
-        matches = []
-        for i in self.items:
-            if i.name == name:
-                matches.append(i)
-        
-        if len(matches) == 0:
-            raise Exception("No such item in stock: " + name)
+    def remove(self, name, quantity):
 
-        matches.sort(key=lambda x: x.price, reverse=True)
+        if name not in self.items:
+            raise ItemNotInStockError(name)
 
-        quantity_stocked = 0
-        for i in matches:
-            quantity_stocked += i.quantity
-        
-        if quantity_stocked < quantity:
-            raise Exception("Not enough to sell. Only " + str(quantity_stocked) + " in stock")
-        
-        payback = 0
-        quantity_removed = 0
-        while quantity_removed < quantity:
-            i = matches[0]
-            if i.quantity >= quantiy:
-                quantity_to_remove = quantity
-            else:
-                quantity_to_remove = quantity - i.quantity
+        i = self.items[name]
 
-            i.quantity -= quantity_to_remove
-            if i.quantity == 0:
-                self.items.remove(i)
-            
-            quantity_removed += quantity_to_remove
-            self.size += size * quantity_to_remove
-            payback += price * quantity
-
-            del matches[0]
+        if i.quantity < quantity:
+            raise WouldUnderflowError(name, i.quantity)
         
-        return payback
+        i.quantity -= quantity
+        if i.quantity == 0:
+            del self.items[name]
+        
+        self.size -= i.unit_size * quantity
+    
--- station_service.py
+++ station_service.py
@@ -1,6 +1,7 @@
 import random
 
 from inventory import Inventory, InventoryItem
+from inventory import Inventory
 
 
 class StationService:
@@ -22,18 +23,25 @@ class Trade(StationService):
         super().__init__(station, "Trade")
 
         self.inventory = Inventory(10000)
-        for i in config.trade_items_stack.items:
+        self.prices = {}
+
+        # pick items at random and fill inventory with random amount of it
+        while self.inventory.size < self.inventory.size_max:
+            i = config.trade_items_stack.items[random.randrange(len(config.trade_items_stack.items))]
+            
+            remaining_space = self.inventory.size_max - self.inventory.size
+            if i.size > remaining_space:
+                continue
+
             chance = random.randint(1, 100)
             if chance <= i.rarity:
+                quantity = remaining_space // i.size
+                self.inventory.add(i.name, i.size, random.randint(1, quantity))
                 twenty_five_percent = i.price * 25 // 100
                 price = random.randint(
                     i.price - twenty_five_percent,
                     i.price + twenty_five_percent)
-                self.inventory.items.append(InventoryItem(
-                    i.name,
-                    random.randint(1,1000),
-                    price,
-                    0))
+                self.prices[i.name] = price
 
 class Research(StationService):
     def __init__(self, station):
