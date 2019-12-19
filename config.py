43fe84259de8323c4f2ca550ecefcedbc00d7e6a
Bering <bering@ringlogic.com>
2019-12-19 10:52:26 -0500

    We need more stations :-)

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ system_height = 24
 # At a distance x around a star, what are the chances one of these shows up (if the previous one didn't)
 probability_planet = 50
 probability_asteroids = 20 # this is the probability for a belt to show up, not just one
-probability_station = 20
+probability_station = 30
 probability_comet = 20
 
 max_planet_per_system = 24 # If you want more you need to provide more names in the Planet class
